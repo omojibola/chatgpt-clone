4a4b596d1a777cfae7dc10983a6a8cb235abc03a
Jibola <omojibolaorija@gmail.com>
2023-06-16 17:22:04 +0100

    fixed login ui

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,92 @@
-import React from 'react';
+import { Logo } from '@/icons';
+import './login.scss';
 
 const page = () => {
-  return <div>login</div>;
+  return (
+    <div className='login-wrapper'>
+      <div className='login-wrapper__logo'>
+        <Logo />
+      </div>
+      <section className='login-wrapper__form-section'>
+        <div className='login-wrapper__form-section__widget'>
+          <div className='login-wrapper__form-section__widget__form'>
+            <header>
+              <h1>Welcome back</h1>
+            </header>
+
+            <form>
+              <div className='input-container'>
+                <input
+                  type='text'
+                  className='text-input'
+                  id='email'
+                  autoComplete='off'
+                  placeholder='Email address'
+                />
+                <label className='label' htmlFor='email'>
+                  Email address
+                </label>
+              </div>
+
+              <button type='submit' className='login-btn'>
+                Continue
+              </button>
+            </form>
+            <div className='alternate-action'>
+              <p className='c7f987bb8 c6338b577 c7e6ea1b7'>
+                Don't have an account?
+                <a className='cad38ddb6 ccef1b889' href='#' aria-label=''>
+                  Sign up
+                </a>
+              </p>
+            </div>
+            <div className='or-span'>
+              <span>Or</span>
+            </div>
+            <div className='other-btns-wrapper'>
+              <button className='other-btns-wrapper__btn'>
+                <span
+                  className='other-btns-wrapper__icon'
+                  data-provider='google'
+                ></span>
+                <span className='other-btns-wrapper__text'>
+                  Continue with Google
+                </span>
+              </button>
+              <button className='other-btns-wrapper__btn'>
+                <span
+                  className='other-btns-wrapper__icon'
+                  data-provider='microsoft'
+                ></span>
+                <span className='other-btns-wrapper__text'>
+                  Continue with Microsoft Account
+                </span>
+              </button>
+
+              <button className='other-btns-wrapper__btn'>
+                <span
+                  className='other-btns-wrapper__icon'
+                  data-provider='apple'
+                ></span>
+                <span className='other-btns-wrapper__text'>
+                  Continue with Apple
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className='i-footer'>
+        <a href='#' target='_blank'>
+          Terms of use
+        </a>{' '}
+        |{' '}
+        <a href='#' target='_blank'>
+          Privacy policy
+        </a>
+      </footer>
+    </div>
+  );
 };
 
 export default page;
---
+++ app/chat/page.tsx
@@ -0,0 +1,12 @@
+import './chat.scss';
+
+const page = () => {
+  return (
+    <div className='chat-wrapper'>
+      <div className='chat-wrapper__sidebar'>sidebar</div>
+      <div className='chat-wrapper__chat-section'>chat page</div>
+    </div>
+  );
+};
+
+export default page;
