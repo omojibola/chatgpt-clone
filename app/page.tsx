96ee4c8978b30049ebb0635cd6dab2ab0e112258
Jibola <omojibolaorija@gmail.com>
2023-06-21 12:41:30 +0100

    removed react typed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+'use client';
 import { Navbar, TextRevealLoop } from '@/components';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/auth/');
+  }, []);
+
   return (
     <main className='nav-hero' style={{ background: '#015453' }}>
       <Navbar />
-      <TextRevealLoop />
     </main>
   );
 }
